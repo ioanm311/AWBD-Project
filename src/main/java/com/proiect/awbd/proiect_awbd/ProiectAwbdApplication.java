38cf96b1868c4eed007bcd83d7d7f236714770f4
Ioan <ioanmarcu77@yahoo.com>
2025-05-05 22:10:55 +0300

    Initial commit

---
+++ src/main/java/com/proiect/awbd/proiect_awbd/ProiectAwbdApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.awbd.proiect_awbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectAwbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectAwbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proiect/awbd/proiect_awbd/ProiectAwbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.awbd.proiect_awbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectAwbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
